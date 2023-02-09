15a8369c85c08a496305f8589d8ac9615bb2f1a5
sr417summit <srividia_ram@yahoo.com>
2023-02-08 19:25:36 -0600

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-ACCOUNT_NAME = "cse6332"
-ACCOUNT_KEY = "6T26i2ByTpJtT1U8DqCiA8PgR78RMeYtmE3QwpRNK4To2Lm98wsz4EFy0iB5L4C82csA5yXI504D+AStvtKt4Q=="
-CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=cse6332;AccountKey=6T26i2ByTpJtT1U8DqCiA8PgR78RMeYtmE3QwpRNK4To2Lm98wsz4EFy0iB5L4C82csA5yXI504D+AStvtKt4Q==;EndpointSuffix=core.windows.net"
+ACCOUNT_NAME = "test1sxr8123"
+ACCOUNT_KEY = "z8Bw5ZkHWIGvl3ki+LPBQefp4I6lQuEuWAH5JQQt9OiQsWidT7ZV5Gh5PfZx49J3IqCUMJI3eUwb+ASthd67nA=="
+CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=test1sxr8123;AccountKey=z8Bw5ZkHWIGvl3ki+LPBQefp4I6lQuEuWAH5JQQt9OiQsWidT7ZV5Gh5PfZx49J3IqCUMJI3eUwb+ASthd67nA==;EndpointSuffix=core.windows.net"
 CONTAINER = "csv"
 #ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg','csv'])
