372aa6c7335f16eab7dd2467796bf1558d991c88
zoudada <1310087583@qq.com>
2023-06-13 08:09:41 +0800

    assign1

--- app.py
+++ app.py
@@ -29,91 +29,79 @@ allowed_ext = app.config['ALLOWED_EXTENSIONS'] # List of accepted extensions
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
 @app.route('/', methods = ["GET","POST"])
-def Name():
-    photo_name = 'car.jpg'
-    image = Image.open(photo_name)
-    data=io.BytesIO()
-    image.save(data,"JPEG")
-    encoded_img_data = base64.b64encode(data.getvalue())
-    p = encoded_img_data.decode('UTF-8')
-    return render_template("Name.html",photo_name=p)
+def index():
+    df = pd.read_csv("./static/people.csv",on_bad_lines='skip')
+    return render_template("index.html",tables=[df.to_html()],titles=['Name','State','Salary','Grade','Room','Telnum','Picture','Keywords'])
+    #return render_template("index.html")
 
-@app.route('/user', methods = ["POST"])
-def user():
-    df = pd.read_csv("q0c.csv",on_bad_lines='skip')
+# def Name():
+#     photo_name = '../car.jpg'
+#     image = Image.open(photo_name)
+#     data=io.BytesIO()
+#     image.save(data,"JPEG")
+#     encoded_img_data = base64.b64encode(data.getvalue())
+#     p = encoded_img_data.decode('UTF-8')
+#     return render_template("Name.html",photo_name=p)
+@app.route('/name',methods = ["GET","POST"])
+def name():
+    df = pd.read_csv("./static/people.csv",on_bad_lines='skip')
     name = request.form["name"]
-    for i in df["name"]:
-        if i !=name:
-            next
-        elif i ==name:
-            pdict = zip(df.name,df.picture)
-            pdict=dict(pdict)
-            state = df[df["name"]==i]["class"].values[0]
-            print("{}'s class is {}".format(i,state)) 
-            photo_name = pdict[i]
-            image = Image.open(photo_name)
-            data=io.BytesIO()
-            image.save(data,"JPEG")
-            encoded_img_data = base64.b64encode(data.getvalue())
-            return render_template('user.html',user=name,state=state,photo_name=encoded_img_data.decode('utf-8')) 
-        return render_template('not_found.html')
+    for i in df["Name"]:
+        if i==name:
+            p=df[df["Name"]==name]["Picture"].values[0]
+            return render_template("index.html",tables=[df.to_html()],titles=['Name','State','Salary','Grade','Room','Telnum','Picture','Keywords'],photo_name=p)
+    return render_template("index.html",tables=[df.to_html()],titles=['Name','State','Salary','Grade','Room','Telnum','Picture','Keywords'],error="Wrong name,Please input again!!!")
+@app.route('/get_salary', methods = ["GET", "POST"])
+def salary_input():
+    return render_template('get_salary.html')
 
-@app.route('/get_room', methods = ["GET", "POST"])
-def room_input():
-    return render_template('get_room.html')
-
-@app.route('/room', methods = ["POST"])
-def room():
-    
+@app.route('/salary', methods = ["POST"])
+def salary():
     df_op = pd.DataFrame()
-    room = request.form["room"].isdigit()
-    df = pd.read_csv("q0c.csv", converters={'room': int},on_bad_lines='skip')
-    #df['income'] = df['income'].fillna(0)
-    #df['income'] = pd.to_numeric(df['income'])
-    df_op = df.loc[df['room'] == room]
-    return render_template('room.html',tables = [df_op.to_html()], titles=['name','pic','descript'])
-@app.route('/get_number', methods = ["GET", "POST"])
-def number_input():
-    return render_template('get_number.html')
-
-@app.route('/number', methods = ["POST"])
-def number():
-    
-    df_op = pd.DataFrame()
-    min1 = request.form["min"].isdigit()
-    max1=request.form["max"].isdigit()
-    df = pd.read_csv("q0c.csv", converters={'teln': int},on_bad_lines='skip')
-    #df['income'] = df['income'].fillna(0)
-    #df['income'] = pd.to_numeric(df['income'])
-    df_op = df.loc[(df['teln']>=min1)& (df['teln']<=max1)]
-    return render_template('number.html',tables = [df_op.to_html()], titles=['name','teln','pic','descript'])
-@app.route('/nameinc', methods = ["GET", "POST"])
-def nameinc_input():
-    return render_template('nameinc.html')
+    sal = request.form["sal"]
+    sal1=request.form["sal1"]
+    df = pd.read_csv("./static/people.csv",on_bad_lines='skip')
+    df_op = df.loc[(df["Salary"]>=sal) & (df["Salary"]<=sal1)]
+    print(df_op["Picture"].values)
+    return render_template('salary.html',tables = [df_op.to_html()], titles=['Name','State','Salary','Grade','Room','Telnum','Picture','Keywords'],photo_name=df_op["Picture"].values)
+@app.route('/get_update', methods = ["GET", "POST"])
+def update_input():
+    return render_template('get_update.html')
+@app.route('/get_delete', methods = ["GET", "POST"])
+def delete_input():
+    return render_template('get_delete.html')
 
 @app.route('/update', methods = ["GET", "POST"])
-def nameinc():
-    df = pd.read_csv("q0c.csv",on_bad_lines='skip')
-    df_op = pd.DataFrame()
-    teln = request.form["teln"]
-    comments = request.form["text"]
-    new_row={'room':'','teln':teln,'pic':'','descript':comments}
-    #df_op = df.loc[df.name == name, ['name','income', 'comments']] = name,inc, comments
-    df = df.append(new_row, ignore_index=True)
-    df.to_csv ("q0c.cs", index = False)
-    return render_template('update.html',tables=[df.to_html()],titles=['name','teln','pic','descript'])
+def update():
+    df = pd.read_csv("./static/people.csv",on_bad_lines='skip')
+    name=request.form["name"]
+    sal=request.form["sal"]
+    word=request.form["word"]
+    index=df.loc[df["Name"]==name].index
+    if index.size==0:
+        error="Wrong name,Please enter again!!!!"
+        return render_template('get_update.html',errors=error) 
+    if sal!="":
+        df.loc[index,"Salary"]=sal
+    if word!="":
+        df.loc[index,"Keywords"]=word
+    inf="Update success!!!!!"
+    return render_template('update.html',tables=[df.to_html()],titles=['Name','State','Salary','Grade','Room','Telnum','Picture','Keywords'],info=inf)
+@app.route('/delete', methods = ["GET", "POST"])
+def delete():
+    df = pd.read_csv("./static/people.csv",on_bad_lines='skip')
+    name=request.form["name"]
+    index=df.loc[df["Name"]==name].index
+    if index.size==0:
+        error="Wrong name,Please enter again!!!!"
+        return render_template('get_delete.html',errors=error) 
+    df_new=df.drop(index[0])
+    inf="delete success!!!!!"
+    return render_template('delete.html',tables=[df_new.to_html()],titles=['Name','State','Salary','Grade','Room','Telnum','Picture','Keywords'],info=inf)
 
-@app.route('/picture', methods = ["GET","POST"])
-def picture():
 
     
-    df = pd.read_csv("q0c.csv",on_bad_lines='skip')
-    df.loc[df.picture == ' ', 'picture'] = 'No_Picture.jpg'
-    df.loc[df.picture == 'Nan', 'picture'] = 'No_Picture.jpg'
-    df.to_csv ("q0c.csv", index = None, header=True)
-    pdict = zip(df.name,df.picture)
-    pdict=dict(pdict)
-    return render_template("picture.html",tables=[df.to_html()],titles=['name','year','comments'],image_name=pdict)
+    
  
 if __name__ == "__main__":
     app.run(debug = True)
